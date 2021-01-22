ebb51d3423d3e8e700f91a7a4c563ca6f94e3587
min486 <67313329+min486@users.noreply.github.com>
2021-01-22 12:43:41 +0900

    RGB 큰 원 가운데 출력 예제

---
+++ RGBcircle_32x64/RGBcircle_32x64.ino
@@ -0,0 +1,62 @@
+// colorwheel demo for Adafruit RGBmatrixPanel library.
+// Renders a nice circle of hues on our 32x32 RGB LED matrix:
+// http://www.adafruit.com/products/607
+// 32x32 MATRICES DO NOT WORK WITH ARDUINO UNO or METRO 328.
+
+// Written by Limor Fried/Ladyada & Phil Burgess/PaintYourDragon
+// for Adafruit Industries.
+// BSD license, all text above must be included in any redistribution.
+
+#include <Adafruit_GFX.h>   // Core graphics library
+#include <P3RGB64x32MatrixPanel.h>
+
+// Most of the signal pins are configurable, but the CLK pin has some
+// special constraints.  On 8-bit AVR boards it must be on PORTB...
+// Pin 11 works on the Arduino Mega.  On 32-bit SAMD boards it must be
+// on the same PORT as the RGB data pins (D2-D7)...
+// Pin 8 works on the Adafruit Metro M0 or Arduino Zero,
+// Pin A4 works on the Adafruit Metro M4 (if using the Adafruit RGB
+// Matrix Shield, cut trace between CLK pads and run a wire to A4).
+
+P3RGB64x32MatrixPanel matrix(25, 26, 27, 21, 22, 0, 15, 32, 33, 12, 16, 17, 4);
+
+void setup() {
+  int      x, y, hue;
+  float    dx, dy, d;
+  uint8_t  sat, val;
+  uint16_t c;
+
+  matrix.begin();
+
+
+  for(y=0; y < matrix.width(); y++) {
+    dy = 15.5 - (float)y;
+    for(x=15; x < matrix.height() +15; x++) {
+    
+      dx = 31 - (float)x;
+      d  = dx * dx + dy * dy;
+      if(d <= (16.5 * 16.5)) { // Inside the circle(ish)?
+        hue = (int)((atan2(-dy, dx) + PI) * 1536.0 / (PI * 2.0));
+        d = sqrt(d);
+        if(d > 15.5) {
+          // Do a little pseudo anti-aliasing along perimeter
+          sat = 255;
+          val = (int)((1.0 - (d - 31)) * 255.0 + 0.5);
+        } else
+        {
+          // White at center
+          sat = (int)(d / 15.5 * 255.0 + 0.5);
+          val = 255;
+        }
+        c = matrix.colorHSV(hue, sat, val);
+      } else {
+        c = 0;
+      }
+      matrix.drawPixel(x, y, c);
+    }
+  }
+}
+
+void loop() {
+  // Do nothing -- image doesn't change
+}

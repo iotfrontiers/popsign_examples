243a06c9a9ae26b7424b76a70ce2b171d1987789
greetdev <fjiogweh@gmail.com>
2021-12-17 14:12:25 +0900

    serialText 폰트부분 제거

--- serialText/serialText.ino
+++ serialText/serialText.ino
@@ -1,7 +1,6 @@
 #include <Adafruit_GFX.h>   
 #include <P3RGB64x32MatrixPanel.h>
-#include <Fonts/GodoM6pt8b.h>
-#include <Fonts/GodoM7pt8b.h>
+
 
 P3RGB64x32MatrixPanel matrix(25, 26, 27, 21, 22, 0, 15, 32, 33, 12, 5, 23, 4);
 
@@ -11,8 +10,7 @@ void setup()
 {
   Serial.begin(115200);
   matrix.begin();
-  matrix.setFont(&GodoM6pt8b);
-  matrix.setAttribute(UTF8_ENABLE , true);  
+
 }
 
 void loop() 
